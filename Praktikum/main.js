b1fadeebdca815039173312f60094c60cc5d343a
Salina Putri Herawati <salinaputriherawati@gmail.com>
2025-03-26 11:38:45 +0700

    Menambahkan file baru

---
+++ Praktikum/main.js
@@ -0,0 +1,83 @@
+// alert('Hallo wasap gengss')//pop up
+// console.log('ini sebuah console')//inspact
+
+// var usia = 21
+// const x = 1
+// let y = 2
+// console.log('umur saya' + usia)
+
+// const usia = 21
+// usia = 23
+// const x = 1
+// let y = 2
+// console.log('umur saya' + usia)
+
+// function contohLet() {
+//     let y = 10;
+//     if (true) {
+//         let y = 20;
+//         console.log("Nilai dalam blok:", y);
+//     }
+//     console.log("Nilai luar blok:", y);
+// }
+// contohLet(); 
+
+// let nama = 'Salina Putri Herawati'//string
+// let usia = 20//integer
+// let tinggiBadan = 160.7//double atau float
+// let beratBadan//double atau float
+// beratBadan = 45.4
+// let uang = 3
+
+// if (uang == null) {
+//     uang = "belum ada"
+// } else (
+//     uang = "udah ada"
+// )
+
+// switch (uang) {
+//     case 1:
+//         uang = "punya sedikit"
+//         break;
+//     case 2:
+//         uang = "punya cukup"
+//         break;
+//     default:
+//         uang = "tidak ada"
+//         break;
+// }
+
+// let uangAwal = 20000
+// let uangTambahan = 15000
+// const uangTotal = uangAwal + uangTambahan//aritmatika
+
+// // alert('nama saya ' + nama)//pop up
+// alert(
+//     `nama saya ${nama}, usia saya ${usia} tahun, tinggi badan saya adalah ${tinggiBadan} cm, berat badan saya ${beratBadan} kg, dan saya ${uang} uang`
+// )
+
+// alert(
+//     `uang awal saya sebesar Rp.${uangAwal} dan saya dikasih uang sama ortu untuk makan siang sebesar Rp.${uangTambahan}, jadi uang saya sekarang sebesar Rp.${uangTotal} `
+// )
+
+// let namaPanggilan = [
+//     'Sal',//index 0
+//     'Selin',//index 1
+//     'Put'//index 2
+// ]
+
+// let mahasiswa = {
+//     nama: "Rina",
+//     usia: 20,
+//     jurusan: "Informatika"
+// }; // Object
+// console.log(mahasiswa.jurusan)
+
+// namaPanggilan.push('Salput', 'Hera')//push: menambahkan isi isi array di luar
+// namaPanggilan.shift()//shift: menghapus salah satu index array di awal
+// namaPanggilan.pop()//pop: menghapus salah satu index array di akhir
+// alert(namaPanggilan)
+
+// console.log(namaPanggilan.length)//menampilkan panjang array
+
+
