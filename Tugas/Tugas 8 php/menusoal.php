5cf52fd8a9763ae35ca307bd7170a3547b8f5410
Salina Putri Herawati <salinaputriherawati@gmail.com>
2025-05-25 15:29:35 +0700

    Menambahkan file baru

---
+++ Tugas/Tugas 8 php/menusoal.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jawaban Soal</title>
+<head>
+<body>
+
+<h1>Menu Soal</h1>
+<ul>
+    <li><a href="latihan_nilai.php"> Tugas 8 Soal 1 : Cek Nilai</a></li>
+    <li><a href="latihanpraktikum2.php"> Tugas 8 Soal 1 : Cek Diskon UKT</a></li>
+<ul>
